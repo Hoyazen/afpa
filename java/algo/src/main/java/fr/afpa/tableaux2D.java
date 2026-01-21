fa73131066ce572d0e0f17b2574da05f6ff0c883
Hoyazen <hoyazenn@gmail.com>
2026-01-21 15:02:31 +0100

    tableaux 2D exo 2

--- java/algo/src/main/java/fr/afpa/tableaux2D.java
+++ java/algo/src/main/java/fr/afpa/tableaux2D.java
@@ -6,7 +6,8 @@ public class tableaux2D {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        analyseAlgorithme1(scanner);
+        // analyseAlgorithme1(scanner);
+        analyseAlgorithme2(scanner);
 
         scanner.close();
     }
@@ -21,6 +22,24 @@ public class tableaux2D {
         for(int i = 0; i < 2; i++) {
             System.out.println("---");
             for(int j = 0; j < 2; j ++) {
+                System.out.println(tableau[i][j]);
+            }
+        }
+    }
+
+    public static void analyseAlgorithme2(Scanner scanner) {
+        int[][] tableau = new int[2][3];
+        int val = 1;
+
+        for(int i = 0; i < 1; i++) {
+            for(int j = 0; j < 2; j++) {
+                tableau[i][j] = val;
+                val = val + 1;
+            }
+        }
+
+        for(int i = 0; i < 1; i++) {
+            for(int j = 0; j < 2; j++) {
                 System.out.println(tableau[i][j]);
             }
         }
