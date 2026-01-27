9f17e7f99bd26c73353703a76a03ea02b1aff67a
Hoyazen <hoyazenn@gmail.com>
2026-01-27 11:15:27 +0100

    algorithmes divers exo 2 V1

--- java/algo/src/main/java/fr/afpa/algorithmesDivers.java
+++ java/algo/src/main/java/fr/afpa/algorithmesDivers.java
@@ -1,9 +1,17 @@
 package fr.afpa;
 
+import java.util.Scanner;
+
 public class algorithmesDivers {
     public static void main(String[] args) {
-        int resultat = pgcd(24, 18);
-        System.out.println("Le plus grand commun diviseur est : " + resultat);
+        Scanner scanner = new Scanner(System.in);
+
+        // int resultat = pgcd(24, 18);
+        // System.out.println("Le plus grand commun diviseur est : " + resultat);
+
+        triangleCaractères(scanner);
+
+        scanner.close();
     }
 
     public static int pgcd(int a, int b) {
@@ -14,5 +22,20 @@ public class algorithmesDivers {
             b = reste;
         }
         return a;
+    }
+
+    public static void triangleCaractères(Scanner scanner) {
+        System.out.println("Combien de niveaux d'étoiles veux-tu ? ");
+        int reponseUtilistateur = scanner.nextInt();
+
+        int compteur = 0;
+
+        String resultat = "";
+
+        while (compteur < reponseUtilistateur) {
+            compteur++;
+            resultat = resultat + "*";
+            System.out.println(resultat);
+        }
     }
 }
