6c8a02c2e1192a4d4cfdb420128fb351bf2e9036
Hoyazen <hoyazenn@gmail.com>
2026-01-28 09:30:59 +0100

    algorithmes divers exo 4

--- java/algo/src/main/java/fr/afpa/algorithmesDivers.java
+++ java/algo/src/main/java/fr/afpa/algorithmesDivers.java
@@ -14,8 +14,10 @@ public class algorithmesDivers {
 
         // trianglesCaracteresV2(scanner);
 
-        isValid(null);
-        System.out.println(isValid(new BigInteger("4223982640269299")));
+        // isValid(null);
+        // System.out.println(isValid(new BigInteger("4223982640269299")));
+
+        validationDates(scanner);
 
         scanner.close();
     }
@@ -57,10 +59,10 @@ public class algorithmesDivers {
             compteur++;
             resultat = resultat + "*";
             System.out.println(resultat);
-        }  
+        }
 
         while (compteur > 0) {
-            compteur --;
+            compteur--;
             resultat = resultat.substring(0, resultat.length() - 1);
             System.out.println(resultat);
         }
@@ -95,4 +97,55 @@ public class algorithmesDivers {
         return somme % 10 == 0;
     }
 
+    public static void validationDates(Scanner scanner) {
+
+        System.out.println("Un jour ?");
+        int reponseJour = scanner.nextInt();
+
+        System.out.println("Un mois ?");
+        int reponseMois = scanner.nextInt();
+
+        System.out.println("Une année ?");
+        int reponseAnnee = scanner.nextInt();
+
+        int jourMax;
+        boolean estBissextile;
+
+        if (reponseAnnee > 2025 || reponseAnnee <= 1930) {
+            System.out.println("Cette année n'est pas valide");
+            return;
+        }
+
+        estBissextile = (reponseAnnee % 400 == 0) ||
+                (reponseAnnee % 4 == 0 && reponseAnnee % 100 != 0);
+
+        if (reponseMois < 1 || reponseMois > 12) {
+            System.out.println("Ce mois n'existe pas");
+            return;
+        }
+
+        if (reponseJour <= 0) {
+            System.out.println("Ce jour n'existe pas");
+            return;
+        }
+
+        if (reponseMois == 1 || reponseMois == 3 || reponseMois == 5 || reponseMois == 7 || reponseMois == 8 || reponseMois == 10 || reponseMois == 12) {
+            jourMax = 31;
+        } else if (reponseMois == 4 || reponseMois == 6 || reponseMois == 9 || reponseMois == 11) {
+            jourMax = 30;
+        } else { //février
+            if (estBissextile) {
+                jourMax = 29;
+            } else {
+                jourMax = 28;
+            }
+        }
+
+        if (reponseJour > jourMax) {
+            System.out.println("Ce jour n'existe pas pour ce mois");
+            return;
+        }
+
+        System.out.println("La date est valide");
+    }
 }
